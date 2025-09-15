5c2e32fe08696590fe96fc8468332a7c49f4b979
user <sooooooobinn@gmail.com>
2025-09-15 16:09:03 +0900

    2025.09.15

---
+++ src/main/java/beakjoon/backjoon2475.java
@@ -0,0 +1,20 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class backjoon2475 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] a = new int[5];
+        int sum = 0;
+        for(int i = 0; i<5; i++){
+            a[i] = in.nextInt();
+        }
+
+        for(int i=0; i<5; i++){
+            sum += a[i]*a[i];
+        }
+        System.out.println(sum%10);
+    }
+
+}
