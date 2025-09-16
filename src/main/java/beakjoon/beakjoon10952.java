d2bc34904bce9cc680cc573cd72583ef3494d7b1
user <sooooooobinn@gmail.com>
2025-09-17 00:05:45 +0900

    2025.09.16

---
+++ src/main/java/beakjoon/beakjoon10952.java
@@ -0,0 +1,20 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class beakjoon10952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+
+        while (true) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            if (a == 0 && b == 0) {
+                break;
+            }
+            System.out.println(a + b);
+        }
+    }
+}
---
+++ src/main/java/beakjoon/beakjoon11654.java
@@ -0,0 +1,12 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class beakjoon11654 {
+    public static void main(String[] args) {;
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+
+
+    }
+}
