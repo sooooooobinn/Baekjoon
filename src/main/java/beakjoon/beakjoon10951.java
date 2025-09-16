3e4fdeb71fa0b7717112c64dadecf36563545e6a
user <sooooooobinn@gmail.com>
2025-09-16 23:55:28 +0900

    2025.09.16

--- src/main/java/beakjoon/beakjoon10951.java
+++ src/main/java/beakjoon/beakjoon10951.java
@@ -6,7 +6,7 @@ public class beakjoon10951 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
+        while (sc.hasNext()) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             System.out.println(a+b);
