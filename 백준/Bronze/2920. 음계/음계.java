cbe66bec5cfc6e1d5d9c9477a19a1ddefd77ce92
sooooooobinn <s25021@gsm.hs.kr>
2025-10-13 08:22:57 +0900

    [Bronze II] Title: 음계, Time: 168 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[8];
+
+        for(int i=0; i<a.length; i++){
+            a[i] = sc.nextInt();
+        }
+
+        boolean ascending = true;
+        boolean descending = true;
+
+        for(int i = 0; i<a.length-1; i++){
+            if(a[i] > a[i+1]){
+                ascending = false;
+            }
+            else if(a[i] < a[i+1]){
+                descending = false;
+            }
+        }
+
+        if(ascending){
+            System.out.println("ascending");
+        }
+        else if(descending){
+            System.out.println("descending");
+        }
+        else{
+            System.out.println("mixed");
+        }
+    }
+}
