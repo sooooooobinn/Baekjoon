bf29d7f44dc089752a43804aa48d2217d12391cb
sooooooobinn <s25021@gsm.hs.kr>
2025-10-13 14:20:13 +0900

    [Bronze II] Title: 나머지, Time: 180 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] main = new int[10];
+        int sum = 0;
+
+        for(int i = 0; i<10; i++) {
+            int n = sc.nextInt();
+            int r = n % 42;
+
+            boolean exist = false;
+
+            for(int j = 0; j < sum; j++) {
+                if(r == main[j]){
+                    exist = true;
+                    break;
+                }
+            }
+
+            if(!exist) {
+                main[sum] = r;
+                sum++;
+            }
+        }
+        System.out.println(sum);
+    }
+}
